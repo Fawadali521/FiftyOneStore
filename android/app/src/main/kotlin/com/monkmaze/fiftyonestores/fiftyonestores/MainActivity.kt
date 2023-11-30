78de18f4f6c230870734b1e3f90df1139aecfa7b
shivay-paradox <shivay.paradox@gmail.com>
2023-11-30 19:48:22 +0530

    init

---
+++ android/app/src/main/kotlin/com/monkmaze/fiftyonestores/fiftyonestores/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.monkmaze.fiftyonestores.fiftyonestores
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:fiftyonestores/LandingPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.top]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '51Stores',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // fontFamily: 'Comforta',
+        scaffoldBackgroundColor: Colors.white,
+        brightness: Brightness.light,
+        useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const LandingPage(),
+    );
+  }
+}
